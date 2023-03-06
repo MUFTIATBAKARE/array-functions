140ee4f5ad5c0954f28229c484435b50371ed2c1
muftiatbakare <bakare.muftiat@gmail.com>
2023-03-07 00:59:27 +0100

    update

--- index.js
+++ index.js
@@ -1,62 +1,73 @@
 /**1)
- * The differences between mutating array methods and non-mutation array methods in JavaScript- Mutating array method alters the original array being called while non-mutating array methods simply returns a new array✌️.
- * The values of arrays assigned using the const keyword can be mutated but the entire array cannot be mutated, this is because non-primitive value are mutable in javascript
+ * The differences between mutating array methods and non-mutation array methods in JavaScript- Mutating array method alters the original array being called upon while non-mutating array methods simply returns a new array.
  * 
- * Examples of mutating array methods are:
+ * Some mutating array methods are:
  * i. pop()
  * ii. push()
  * iii. shift()
  * iv. sort()
  * v. reverse()
  *  
- * Examples of non-mutation array methods are:
+ * Some non-mutation array methods are:
  * i. slice()
  * ii. concat()
  * iii. map()
  * iv. filter()
- * v. ()
+ * v. flat()
  * */ 
 
 
 // 2
 const language = ['C#', 'Javascript', 'Ruby', 'PHP', 'Python'];
 console.log(language);
+
 language.push('Kotlin');
 console.log(language);
+
 language.splice(3,0,'Java');
 console.log(language);
+
 language.shift();
 console.log(language);
+
 language.unshift('Scala', 'Swift');
 console.log(language);
+
 language.splice(5,1,'Go','Rust');
 console.log(language);
 
-// 3
-// let fruit = ['apple', 'mango', 'banana'];
-// 		function changeFruit( fruit ) {
-//     			fruit[2] = "orange";
-//     			return fruit;
-// 		}
-// The value of fruit will be ['apple', 'mango', 'orange']
+// // 3
+// // let fruit = ['apple', 'mango', 'banana'];
+// // 		function changeFruit( fruit ) {
+// //     			fruit[2] = "orange";
+// //     			return fruit;
+// // 		}
+// // The value of fruit will be ['apple', 'mango', 'orange']
 
 
-// 4
-const arrayOfNumbers = [4,0.5,-89,54]
-const max = arrayOfNumbers.reduce((a,b) => Math.max(a, b), -10000)
+// // 4 using arrow function
+
+const arrayOfNumbers = [-4,0.5,89,54]
+const max = arrayOfNumbers.reduce((value,index) => Math.max(value, index), -100)
 console.log("Maximum value is " + max);
 
+// or
+const arrayOfNumbers2 = [40,0.8,9,-54]
+const max2 = arrayOfNumbers2.reduce(function(value,index){
+  return Math.max(value,index)
+},-100);
+console.log("Maximum value is " + max2);
 
-// 5
+
+// // 5
+
 const arrOfNumbers = [4, 8, 5];
-const valTimesIndex = arrOfNumbers.map(function(num){
-    let result = [];
-    let index = 0;
-    while (index < arrOfNumbers.length) {
-    let x = num * index;
-    index++;
-    result.push(x);
-  }
-    return result;
-})
+const valTimesIndex = arrOfNumbers.map(function(value,index){
+  return value * index
+});
 console.log(valTimesIndex);
+
+// or using arrow function
+const arrOfNumbers2 = [8, 60, 35];
+const valTimesIndex2 = arrOfNumbers2.map((value,index) => value * index);
+console.log(valTimesIndex2);
